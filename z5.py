962fb826e0ac3cdf1f05baf412c64f7082dc1cc0
abakuntzz <148979166+abakuntzz@users.noreply.github.com>
2024-02-03 13:44:58 +0000

    занаие 5

---
+++ z5.py
@@ -0,0 +1,24 @@
+import csv
+def hash(game,name):
+    '''Создаёт хэш персонажа на основе его имени(name) и названия игры(game)'''
+    p=65
+    s=game.replace(' ','')+name
+    m = 10**9+9
+    res=0
+    alp='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    for i in range(len(s)):
+        res+=(alp.find(s[i])*p**i)%m
+    return res
+file=open('game.txt')
+data=file.read().split('\n')
+datanew=[]
+filenew=open('game_with_hash.csv','w')
+writer=csv.writer(filenew)
+t=data.pop(0).split('$')
+header=['hash',t[0],t[1],t[2],t[3]]
+writer.writerow(header)
+for i in data:
+    s=i.split('$')
+    datanew.append(s)
+    newline=[hash(s[0],s[1]),s[0],s[1],s[2],s[3]]
+    writer.writerow(newline)
